5e20cf4845a92757db7fb8a602d25170fcd9f52d
nikolaystanishev <stanishev.nikolay@gmail.com>
2021-10-25 16:51:03 +0300

    Add path params

--- 2021-10-21/servlets/src/main/java/org/elsys/EchoServlet.java
+++ 2021-10-21/servlets/src/main/java/org/elsys/EchoServlet.java
@@ -7,13 +7,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "EchoServlet", urlPatterns = "/echo")
+@WebServlet(name = "EchoServlet", urlPatterns = "/echo/*")
 public class EchoServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String msg = req.getParameter("msg");
+        String pathInfo = req.getPathInfo(); // /{value}/test
+        String[] pathParts = pathInfo.split("/");
 
         resp.setStatus(200);
-        resp.getWriter().println("echo: " + msg);
+        resp.getWriter().println("echo: " + msg + " " + String.join(" ", pathParts));
     }
 }
